8aca8efd30037b6873e712b6cb5b5c9d4fbb5a5b
Alex Johnson <ajohnson2389@gmail.com>
2020-09-29 10:09:15 -0400

    created logic for calculating bmi

---
+++ lib/calculator_brain.dart
@@ -0,0 +1,25 @@
+import 'dart:math';
+
+class CalculatorBrain {
+  CalculatorBrain({this.height, this.weight});
+
+  final int height;
+  final int weight;
+
+  double _bmi; //Make it private
+
+  String calculateBMI() {
+    double _bmi = (weight / pow(height, 2)) * 703;
+    return _bmi.toStringAsFixed(1);
+  }
+
+  String getResult() {
+    if (_bmi >= 25) {
+      return 'Overweight';
+    } else if (_bmi > 18.5) {
+      return 'Normal';
+    } else {
+      return 'Underweight';
+    }
+  }
+}
